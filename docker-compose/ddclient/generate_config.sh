c79e407267f82dbf566799f981b1f271b04c7c3e
Zakaria Fadli <zakaria.fadli@netatmo.com>
2021-07-05 23:48:51 +0200

    add ddclient config generator script

---
+++ docker-compose/ddclient/generate_config.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+CONFIG_ENV="$SCRIPT_DIR/config"
+
+echo """protocol=dyndns2
+server=dynv6.com
+login=none
+password='opqtuVeUAW8-wTa8vcxAkTszrgygAk' """ > $CONFIG_ENV
+# FIXME hide password using git-crypt
+
+case $(cat /etc/machine-id) in
+  "8c32ed2befb945bcb5bcf26daf1f864d")
+    echo Generating config for pi4 at Home in Issy
+    URLS="www.zakariafadli.com, transmission.zakariafadli.com, hass.zakariafadli.com, portainer.zakariafadli.com, media.zakariafadli.com"
+    ;;
+
+  "a80304ee19f1405f8695cc2de9dfd9fa") # pi2B in morocco
+    echo Generating config for pi2 at Home in Issy
+    URLS="sidirahal.zakariafadli.com"
+    ;;
+
+  *)
+    echo unknown host. incomplete config genrated.
+    URLS=""
+    ;;
+esac
+
+echo "$URLS" >> $CONFIG_ENV
+
