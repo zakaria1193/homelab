b457d0252f34a6433eb99cd6efd7c45348a41ff4
Zakaria Fadli <zakaria1193@gmail.com>
2026-08-05 12:29:10 +0200

    Retire some services

--- services/cloudfared/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-armhf.deb && 
-
-sudo dpkg -i cloudflared.deb && 
-
-sudo cloudflared service install $(cat ./.token.txt)
